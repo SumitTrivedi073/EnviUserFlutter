709658d7c3c7444e90b3103318d59b2ed55852ae
sumit <sumittrivedi073@gmail.com>
2022-07-19 17:52:11 +0530

    1) pull from master and add navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:envi/UiWidget/cardbanner.dart';
-import 'package:envi/theme/Theme.dart';
+import 'package:envi/theme/theme.dart';
 import 'package:envi/theme/responsive.dart';
 import 'package:envi/web_service/Constant.dart';
 import 'package:flutter/material.dart';
@@ -17,22 +17,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Envi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MaterialApp(
@@ -41,11 +31,8 @@ class MyApp extends StatelessWidget {
         theme: appTheme(),
         home: MainEntryPoint(),
         routes: {
-
           pageRoutes.login: (context) => Loginpage(),
           pageRoutes.setting: (context) => HomePage(title: "title"),
-
-
         },
       )
     );
@@ -53,15 +40,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MainEntryPoint extends StatefulWidget {
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<MainEntryPoint> createState() => _MainEntryPointState();
 }
@@ -72,8 +50,6 @@ class _MainEntryPointState extends State<MainEntryPoint> {
   @override
   void initState() {
     super.initState();
-    // firestoreConn(setLiveDataStream);
-
     checkLoginStatus();
   }
 
@@ -84,10 +60,7 @@ class _MainEntryPointState extends State<MainEntryPoint> {
           MaterialPageRoute(builder: (BuildContext context) => Loginpage()),
               (Route<dynamic> route) => false);
     } else {
-      //firestoreConnAlerts();
-
       Navigator.of(context).pushAndRemoveUntil(
-        // MaterialPageRoute(builder: (BuildContext context) => MainPage()),
           MaterialPageRoute(
               builder: (BuildContext context) => HomePage(title: "title")),
               (Route<dynamic> route) => false);
