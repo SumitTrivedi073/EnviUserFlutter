28cef7aa4903ccbe34b1b410045d7a2b03377729
sumit <sumittrivedi073@gmail.com>
2022-09-08 18:06:19 +0530

    1)Map Direction Widget Design and functionality development

---
+++ lib/uiwidget/mapDirectionWidget.dart
@@ -0,0 +1,107 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../web_service/Constant.dart';
+
+class MapDirectionWidget extends StatefulWidget{
+  @override
+  _MapDirectionWidgetState createState() => _MapDirectionWidgetState();
+}
+
+class _MapDirectionWidgetState extends State<MapDirectionWidget> {
+
+  GoogleMapController? mapController; //contrller for Google map
+  PolylinePoints polylinePoints = PolylinePoints();
+
+  String googleAPiKey = GoogleApiKey;
+
+  Set<Marker> markers = Set(); //markers for google map
+  Map<PolylineId, Polyline> polylines = {}; //polylines to show direction
+
+  LatLng startLocation = const LatLng(13.199521326936647, 77.70186503653797);
+  LatLng endLocation = const LatLng(13.201484637508047, 77.6612178079766);
+
+
+  @override
+  void initState() {
+
+    markers.add(Marker( //add start location marker
+      markerId: MarkerId(startLocation.toString()),
+      position: startLocation, //position of marker
+      infoWindow: const InfoWindow( //popup info
+        title: 'Starting Point ',
+        snippet: 'Start Marker',
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen), //Icon for Marker
+    ));
+
+    markers.add(Marker( //add distination location marker
+      markerId: MarkerId(endLocation.toString()),
+      position: endLocation, //position of marker
+      infoWindow: const InfoWindow( //popup info
+        title: 'Destination Point ',
+        snippet: 'Destination Marker',
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed), //Icon for Marker
+    ));
+
+    getDirections(); //fetch direction polylines from Google API
+
+    super.initState();
+  }
+
+  getDirections() async {
+    List<LatLng> polylineCoordinates = [];
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleAPiKey,
+      PointLatLng(startLocation.latitude, startLocation.longitude),
+      PointLatLng(endLocation.latitude, endLocation.longitude),
+      travelMode: TravelMode.driving,
+    );
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    } else {
+      print(result.errorMessage);
+    }
+    addPolyLine(polylineCoordinates);
+  }
+
+  addPolyLine(List<LatLng> polylineCoordinates) {
+    PolylineId id = const PolylineId("poly");
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.green,
+      points: polylineCoordinates,
+      width: 5,
+    );
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: GoogleMap( //Map widget from google_maps_flutter package
+        zoomGesturesEnabled: true, //enable Zoom in, out on map
+        initialCameraPosition: CameraPosition( //innital position in map
+          target: startLocation, //initial position
+          zoom: 16.0, //initial zoom level
+        ),
+        markers: markers, //markers to show on map
+        polylines: Set<Polyline>.of(polylines.values), //polylines
+        mapType: MapType.normal, //map type
+        onMapCreated: (controller) { //method called when map is created
+          setState(() {
+            mapController = controller;
+          });
+        },
+      ),
+    );
+  }
+}
--- lib/web_service/Constant.dart
+++ lib/web_service/Constant.dart
@@ -6,6 +6,7 @@ var LoginEmail = "email";
 var Loginpropic = "propic";
 var Logingender = "gender";
 var Loginphone = "phone";
+var GoogleApiKey = "AIzaSyAMnSO4iTYphqjRAnu80OG0FNLt1mvQe3c";
 
 var placeHolderImage =
     "https://play-lh.googleusercontent.com/33VZ5V5cOf0td4-tMvaFmtFErja4nURmwhQG5_UvoPQRTQDPBhrwNASAm6rvmbm6EL8=w480-h960-rw";
