46ed90afa5a5bfb54254a900d3344ac5b908d4f9
Aryan Singh <aryansingh729@gmail.com>
2022-09-19 12:32:54 +0530

    added fromaddress model

---
+++ lib/sidemenu/pickupDropAddressSelection/model/fromAddressModel.dart
@@ -0,0 +1,16 @@
+
+import 'dart:convert';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class FromAddressLatLong {
+
+FromAddressLatLong({
+  required this.address,
+   this.position,
+});
+  String address;
+  LatLng? position;
+  
+}
