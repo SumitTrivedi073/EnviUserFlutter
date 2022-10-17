4d53cfedb4225908ae42b1c8aeaae858ffc32f93
sumit <sumittrivedi073@gmail.com>
2022-10-17 18:54:58 +0530

    1) Add Expandable Bottom Sheet

--- lib/sidemenu/waitingForDriverScreen/waitingForDriverScreen.dart
+++ lib/sidemenu/waitingForDriverScreen/waitingForDriverScreen.dart
@@ -6,6 +6,7 @@ import 'package:envi/uiwidget/mapPageWidgets/mapDirectionWidgetPickup.dart';
 import 'package:envi/uiwidget/otpViewWidget.dart';
 import 'package:envi/uiwidget/timerbutton.dart';
 import 'package:envi/web_service/Constant.dart';
+import 'package:expandable_bottom_sheet/expandable_bottom_sheet.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -20,6 +21,7 @@ import '../onRide/onRideWidget.dart';
 
 class WaitingForDriverScreen extends StatefulWidget {
   final GlobalKey<MapDirectionWidgetPickupState> _key = GlobalKey();
+
   WaitingForDriverScreen({Key? key}) : super(key: key);
 
   @override
@@ -27,8 +29,12 @@ class WaitingForDriverScreen extends StatefulWidget {
 }
 
 class _WaitingForDriverScreenState extends State<WaitingForDriverScreen> {
+  GlobalKey<ExpandableBottomSheetState> key = new GlobalKey();
   late String duration = "0 Minute";
   bool isLoaded = false;
+  int _contentAmount = 0;
+  ExpansionStatus _expansionStatus = ExpansionStatus.contracted;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -60,12 +66,106 @@ class _WaitingForDriverScreenState extends State<WaitingForDriverScreen> {
           return value.liveTripData != null
               ? Scaffold(
                   body: Stack(alignment: Alignment.center, children: <Widget>[
-                  MapDirectionWidgetPickup(
+
+                    MapDirectionWidgetPickup(
                     key: widget._key,
                     liveTripData: value.liveTripData!,
                     callback: retrieveDuration,
                   ),
-                  Column(children: [
+                    ExpandableBottomSheet(
+                      //use the key to get access to expand(), contract() and expansionStatus
+                      key: key,
+
+                      //optional
+                      //callbacks (use it for example for an animation in your header)
+                      onIsContractedCallback: () => print('contracted'),
+                      onIsExtendedCallback: () => print('extended'),
+
+                      //optional; default: Duration(milliseconds: 250)
+                      //The durations of the animations.
+                      animationDurationExtend: const Duration(milliseconds: 500),
+                      animationDurationContract:
+                      const Duration(milliseconds: 250),
+
+                      //optional; default: Curves.ease
+                      //The curves of the animations.
+                      animationCurveExpand: Curves.bounceOut,
+                      animationCurveContract: Curves.ease,
+
+                      //optional
+                      //The content extend will be at least this height. If the content
+                      //height is smaller than the persistentContentHeight it will be
+                      //animated on a height change.
+                      //You can use it for example if you have no header.
+
+                      //required
+                      //This is the widget which will be overlapped by the bottom sheet.
+                      background: Container(
+                        color: Colors.transparent,
+                      ),
+
+                      //optional
+                      //This widget is sticking above the content and will never be contracted.
+                      persistentHeader: GestureDetector(
+                        onTap: () {
+                          if (_expansionStatus == ExpansionStatus.contracted) {
+                            setState(() {
+                              key.currentState!.expand();
+                              _expansionStatus =
+                                  key.currentState!.expansionStatus;
+                            });
+                          } else {
+                            setState(() {
+                              key.currentState!.contract();
+                              _expansionStatus =
+                                  key.currentState!.expansionStatus;
+                            });
+                          }
+                        },
+                        child: Container(
+                          color: Colors.green,
+                          constraints: const BoxConstraints.expand(height: 40),
+                          child: Center(
+                            child: Container(
+                              height: 8.0,
+                              width: 50.0,
+                              color:
+                              Color.fromARGB((0.25 * 255).round(), 0, 0, 0),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      //required
+                      //This is the content of the bottom sheet which will be extendable by dragging.
+                      expandableContent: SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(top: 10),
+                              child: TimerButton(
+                                liveTripData: value.liveTripData!,
+                              ),
+                            ),
+                            DriverDetailWidget(
+                              duration: duration,
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // optional
+                      // This will enable tap to toggle option on header.
+                      enableToggle: true,
+
+                      //optional
+                      //This is a widget aligned to the bottom of the screen and stays there.
+                      //You can use this for example for navigation.
+                      persistentFooter: FromToData(value.liveTripData!),
+                    ),
+
+                    Column(children: [
                     const AppBarInsideWidget(
                       title: "Envi",
                       isBackButtonNeeded: false,
@@ -76,14 +176,6 @@ class _WaitingForDriverScreenState extends State<WaitingForDriverScreen> {
                       alignment: Alignment.topRight,
                       child: OTPView(otp: value.liveTripData!.tripInfo.otp),
                     ),
-                    const Spacer(),
-                    TimerButton(
-                      liveTripData: value.liveTripData!,
-                    ),
-                    DriverDetailWidget(
-                      duration: duration,
-                    ),
-                    FromToData(value.liveTripData!),
                   ]),
                 ]))
               : Container();
