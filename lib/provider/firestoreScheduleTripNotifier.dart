2bbf22ccfe643ed5391ec2711e33ccf53a21cfea
sumit <sumittrivedi073@gmail.com>
2022-10-22 15:59:01 +0530

    1) removing all previous day's schedule trip node from firestore

--- lib/provider/firestoreScheduleTripNotifier.dart
+++ lib/provider/firestoreScheduleTripNotifier.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:envi/appConfig/Profiledata.dart';
 import 'package:envi/sidemenu/upcomingride/model/ScheduleTripModel.dart';
 import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
 
 import '../utils/utility.dart';
 import '../web_service/Constant.dart';
@@ -28,9 +29,26 @@ class firestoreScheduleTripNotifier extends ChangeNotifier {
 
           var encodedJson = json.encode(data, toEncodable: myEncode);
           var jsonData = json.decode(encodedJson);
-          print("scheduleTripModel========> ${res.doc.data()}");
+          DateTime date = DateTime.parse(res.doc['scheduledAt'].toDate().toString());
+
+          String dt1 = DateFormat.yMd().format(date);
+          String dt2 = DateFormat.yMd().format(DateTime.now());
+          DateTime? scheduleDate = DateFormat.yMd().parse(dt1);
+          DateTime? currentDate = DateFormat.yMd().parse(dt2);
+
           if(jsonData!=null && jsonData.toString().isNotEmpty) {
-            scheduleTrip = ScheduleTrip.fromJson(jsonData);
+            if(scheduleDate.isAtSameMomentAs(currentDate)|| scheduleDate.isAfter(currentDate)) {
+              print("scheduleTripModel========> ${res.doc['scheduledAt']}");
+
+              scheduleTrip = ScheduleTrip.fromJson(jsonData);
+            }else{
+              scheduleTrip = null;
+              collectionRef.doc(res.doc.id).delete().then(
+                    (doc) => print("Document deleted"),
+                onError: (e) => print("Error updating document $e"),
+              );
+
+            }
           } else {
             scheduleTrip = null;
           }
