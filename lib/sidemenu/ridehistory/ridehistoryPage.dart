7ffff54b96c54d2a05198a33d3211e36c6f3d9e6
RAGHU <kavunni1976@gmail.com>
2022-10-28 09:07:04 +0530

    carbon profile

--- lib/sidemenu/ridehistory/ridehistoryPage.dart
+++ lib/sidemenu/ridehistory/ridehistoryPage.dart
@@ -19,6 +19,14 @@ import '../../web_service/Constant.dart';
 import '../onRide/model/SosModel.dart';
 import 'model/rideHistoryModel.dart';
 
+
+class CarbonProfile {
+  late double totalLifeTimeKMs;
+  late double totalLifeTimeTrips;
+  late String carbonSaved;
+  
+
+}
 class RideHistoryPage extends StatefulWidget {
   @override
   State<RideHistoryPage> createState() => _RideHistoryPageState();
@@ -33,6 +41,8 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
   int _limit = 20;
   late dynamic userId;
   List<RideHistoryModel> arrtrip = [];
+
+  dynamic carbonProfile;
 
   @override
   void initState() {
@@ -57,9 +67,11 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
     dynamic res = await HTTP.get(getUserTripHistory(userId, pagecount, _limit));
     if (res != null && res.statusCode != null && res.statusCode == 200) {
       setState(() {
-
         //print(jsonDecode(res.body)['content']);
         if (jsonDecode(res.body)['content']['result'] != null) {
+          carbonProfile = jsonDecode(res.body)['content']['carbonProfile'];
+          print(
+              'RAGHU CARBON PROFILE ${carbonProfile} '); //totalLifeTimeKMs, totalLifeTimeTrips, carbonSaved
           arrtrip = (jsonDecode(res.body)['content']['result'] as List)
               .map((i) => RideHistoryModel.fromJson(i))
               .toList();
@@ -71,7 +83,6 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
       });
     }
     setState(() {
-
       if (arrtrip.length != _limit) {
         _hasNextPage = false;
       }
@@ -178,7 +189,8 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
   }
 
   Widget _buildPosts(BuildContext context) {
-    if(arrtrip.length == 0 || arrtrip == null)return Center(child: Text("No trips data available"));
+    if (arrtrip.length == 0 || arrtrip == null)
+      return Center(child: Text("No trips data available"));
     return InkWell(
         onTap: () {
           //onSelectTripDetailPage(context);
@@ -238,17 +250,19 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
               fontWeight: FontWeight.bold,
             ),
           ]),
-          arrtrip[index].price.totalFare!='NA'? robotoTextWidget(
-            textval: "₹ ${arrtrip[index].price.totalFare}",
-            colorval: AppColor.black,
-            sizeval: 18.0,
-            fontWeight: FontWeight.bold,
-          ): robotoTextWidget(
-            textval: arrtrip[index].status,
-            colorval: AppColor.black,
-            sizeval: 18.0,
-            fontWeight: FontWeight.bold,
-          ),
+          arrtrip[index].price.totalFare != 'NA'
+              ? robotoTextWidget(
+                  textval: "₹ ${arrtrip[index].price.totalFare}",
+                  colorval: AppColor.black,
+                  sizeval: 18.0,
+                  fontWeight: FontWeight.bold,
+                )
+              : robotoTextWidget(
+                  textval: arrtrip[index].status,
+                  colorval: AppColor.black,
+                  sizeval: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
         ],
       ),
     );
@@ -278,13 +292,13 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
                       ),
                       Container(
                         width: MediaQuery.of(context).size.width - 190,
-                      child: robotoTextWidget(
-                        textval: arrtrip[index].toAddress,
-                        colorval: AppColor.black,
-                        sizeval: 14.0,
-                        fontWeight: FontWeight.normal,
-                      ),),
-
+                        child: robotoTextWidget(
+                          textval: arrtrip[index].toAddress,
+                          colorval: AppColor.black,
+                          sizeval: 14.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
                     ],
                   ),
                   const SizedBox(
@@ -297,36 +311,34 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
                       ),
                       Container(
                         width: MediaQuery.of(context).size.width - 190,
-                        child:
-                      robotoTextWidget(
-                        textval: arrtrip[index].fromAddress,
-                        colorval: AppColor.greyblack,
-                        sizeval: 14.0,
-                        fontWeight: FontWeight.normal,
-                      ),)
+                        child: robotoTextWidget(
+                          textval: arrtrip[index].fromAddress,
+                          colorval: AppColor.greyblack,
+                          sizeval: 14.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      )
                     ],
                   ),
                 ],
               ),
               Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(
-                    encodeImgURLString(arrtrip[index].driverPhoto),
-                    fit: BoxFit.fill,
-                    height: 40,
-                    width: 40,
-                  ),
-
-                  robotoTextWidget(
-                    textval: arrtrip[index].vehicle.Vnumber,
-                    colorval: AppColor.darkgrey,
-                    sizeval: 13.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ]),
-
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.network(
+                      encodeImgURLString(arrtrip[index].driverPhoto),
+                      fit: BoxFit.fill,
+                      height: 40,
+                      width: 40,
+                    ),
+                    robotoTextWidget(
+                      textval: arrtrip[index].vehicle.Vnumber,
+                      colorval: AppColor.darkgrey,
+                      sizeval: 13.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ]),
             ],
           ),
           const SizedBox(
@@ -339,12 +351,14 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
                 const Padding(
                   padding: EdgeInsets.only(left: 25),
                 ),
-                arrtrip[index].distance!='NA'?robotoTextWidget(
-                  textval: "${arrtrip[index].distance} Km",
-                  colorval: AppColor.darkgrey,
-                  sizeval: 13.0,
-                  fontWeight: FontWeight.w800,
-                ):Container(),
+                arrtrip[index].distance != 'NA'
+                    ? robotoTextWidget(
+                        textval: "${arrtrip[index].distance} Km",
+                        colorval: AppColor.darkgrey,
+                        sizeval: 13.0,
+                        fontWeight: FontWeight.w800,
+                      )
+                    : Container(),
               ]),
               robotoTextWidget(
                 textval: arrtrip[index].name,
@@ -369,23 +383,24 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
               bottomLeft: Radius.circular(10),
               bottomRight: Radius.circular(10)),
           border: Border.all(color: AppColor.border, width: 1.0)),
-      child: arrtrip[index].status == RideHistoryCancelledStatus && arrtrip[index].status == RideHistoryRejectedStatus
+      child: arrtrip[index].status == RideHistoryCancelledStatus &&
+              arrtrip[index].status == RideHistoryRejectedStatus
           ? Container(
-             width: MediaQuery.of(context).size.width,
-        child: MaterialButton(
-          child: robotoTextWidget(
-            textval: Support,
-            colorval: AppColor.butgreen,
-            sizeval: 14.0,
-            fontWeight: FontWeight.bold,
-          ),
-          onPressed: () {
-            makingPhoneCall(LandingPageConfig().getcustomerCare() != null
-                ? LandingPageConfig().getcustomerCare()
-                : '');
-          },
-        ),
-      )
+              width: MediaQuery.of(context).size.width,
+              child: MaterialButton(
+                child: robotoTextWidget(
+                  textval: Support,
+                  colorval: AppColor.butgreen,
+                  sizeval: 14.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                onPressed: () {
+                  makingPhoneCall(LandingPageConfig().getcustomerCare() != null
+                      ? LandingPageConfig().getcustomerCare()
+                      : '');
+                },
+              ),
+            )
           : Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
               Align(
                 alignment: Alignment.center,
@@ -459,7 +474,7 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
                   child: Column(
                     children: const [
                       robotoTextWidget(
-                        textval: "22",
+                        textval: "22", //Change here
                         colorval: AppColor.white,
                         sizeval: 22.0,
                         fontWeight: FontWeight.bold,
@@ -477,7 +492,7 @@ class _RideHistoryPageState extends State<RideHistoryPage> {
                   child: Column(
                     children: const [
                       robotoTextWidget(
-                        textval: "175 Kg",
+                        textval: "175 Kg", //Change Here
                         colorval: AppColor.white,
                         sizeval: 22.0,
                         fontWeight: FontWeight.bold,
